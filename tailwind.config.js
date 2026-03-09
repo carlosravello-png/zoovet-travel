fc0c5f3050dd45d2566eb95c7c42bb1089ab489f
Carlos Ravello <carlosravello@gmail.com>
2026-03-09 05:59:51 -0500

    Aquí pones lo que realmente hiciste

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './articles/**/*.html',
+    './articulos-interes/**/*.html',
+    './zoopedia/**/*.html',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
